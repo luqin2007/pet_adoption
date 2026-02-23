1b73e425e4163799b2fe2a4394af267fcab169a2
luqin2007 <lq2007lq@hotmail.com>
2026-02-23 19:35:47 +0800

    chore: install and register pinia

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).use(ElementPlus).mount('#app')
+createApp(App).use(createPinia()).use(router).use(ElementPlus).mount('#app')
